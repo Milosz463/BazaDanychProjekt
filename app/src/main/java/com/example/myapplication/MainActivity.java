6bc9c82945b437afad3944c954c6538461deea5a
Milosz463 <miloszdawiec6@gmail.com>
2025-04-03 08:56:43 +0200

    fwfwfw

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+private BazaDanychPiosenek bazaDanychPiosenek;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RoomDatabase.Callback callback=new RoomDatabase.Callback() {
+            @Override
+            public void onCreate(@NonNull SupportSQLiteDatabase db) {
+                super.onCreate(db);
+            }
+
+            @Override
+            public void onOpen(@NonNull SupportSQLiteDatabase db) {
+                super.onOpen(db);
+            }
+        };
+        bazaDanychPiosenek= Room.databaseBuilder(getApplicationContext(), BazaDanychPiosenek.class,"Piosenki")
+                .addCallback(callback)
+                .allowMainThreadQueries()
+                .build();
+        WypiszPiosenki();
+    }
+    private void WypiszPiosenki(){
+        bazaDanychPiosenek.getDaoMuzyka().dodajPiosenke(new Muzyka("fwfwf","fwafafaf",2000,0));
     }
 }
